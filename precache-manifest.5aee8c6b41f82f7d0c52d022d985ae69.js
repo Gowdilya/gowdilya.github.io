377f11f7d5982aad5abbffe8232c5b6cda79f7b3
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro.local>
2020-05-10 23:34:37 -0400

    Updates

---
+++ precache-manifest.5aee8c6b41f82f7d0c52d022d985ae69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869ba7344d7df14b966899d102e7cbac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aba889a21c3684fb1310",
+    "url": "./static/css/main.1dc355c6.chunk.css"
+  },
+  {
+    "revision": "24001ae7279ada5a35e8",
+    "url": "./static/js/2.f694ac42.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "./static/js/2.f694ac42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba889a21c3684fb1310",
+    "url": "./static/js/main.7e119805.chunk.js"
+  },
+  {
+    "revision": "88b6dfad04c720a11f26",
+    "url": "./static/js/runtime-main.f55a98c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
